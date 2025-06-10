baf55eaeea2c29cf7ba36d26ce686bb9a269c0ac
Praise <praisexi@outlook.com>
2025-06-10 23:50:54 +0100

    Add @vercel/analytics dependency and integrate Analytics component in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from '../components/Header';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className} style={{overflowX: "hidden"}}>
         <Header />
         {children}
+        <Analytics />
       </body>
     </html>
   )
